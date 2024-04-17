8d30a9699e24f093bf13ded0b456600031b0e8bb
caue-oliver <caue.ooliver@gmail.com>
2024-04-17 16:44:21 -0300

    projeto finalizado

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class hero{
-    constructor
+    constructor(name,age,type){
+        this.name=name
+        this.age=age
+        this.type=type
+    }
+    getHeroAttack(){
+        this.attack
+        switch (this.type) {
+            case "guerreiro":this.attack="espada"
+                break;
+            case "mago":this.attack="magia"
+                break;
+            case "monge":this.attack="artes marciais"
+                break;
+            case "ninja":this.attack="shuriken"
+                break;
+            default: this.attack="und"
+                break;
+        }
+        return this.attack
+    }
+    showHeroAttack(){
+        console.log(`O ${this.type} atacou usando ${ this.attack}`)
+    }
 }
+let character= new hero("cauê",18,"mago")
+character.getHeroAttack()
+character.showHeroAttack()
